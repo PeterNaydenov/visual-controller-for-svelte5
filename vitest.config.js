8673998ba4c899d9ef89ba0fdbdc271662711e27
Peter Naydenov <peter.naydenov@gmail.com>
2026-04-13 17:56:00 +0300

    Test. Add code coverage

--- vitest.config.js
+++ vitest.config.js
@@ -1,7 +1,14 @@
 import { defineConfig } from 'vitest/config';
+import { svelte } from '@sveltejs/vite-plugin-svelte';
 
 export default defineConfig({
+  plugins: [svelte()],
   test: {
     environment: 'jsdom',
+    coverage: {
+      provider: 'v8',
+      reporter: ['text', 'json', 'html', 'lcov', 'cobertura'],
+      include: ['src/**/*.js'],
+    },
   },
 });
