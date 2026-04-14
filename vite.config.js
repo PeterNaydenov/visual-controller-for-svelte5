218ddc4079c044df22edee5c9da4beebec7cec6b
Peter Naydenov <peter.naydenov@gmail.com>
2026-04-14 11:55:27 +0300

    Build a esm, commonjs and umd modules

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,32 @@
 import { defineConfig } from 'vite'
 import { svelte } from '@sveltejs/vite-plugin-svelte'
+import { resolve } from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [svelte()],
+  build: {
+    lib: {
+      entry: resolve(__dirname, 'src/main.js'),
+      name: 'VisualControllerSvelte5',
+      formats: ['es', 'cjs', 'umd'],
+      fileName: (format) => {
+        const map = {
+          es: 'visualController-svelte-5.esm.mjs',
+          cjs: 'visualController-svelte-5.cjs',
+          umd: 'visualController-svelte-5.umd.js'
+        }
+        return map[format]
+      }
+    },
+    rollupOptions: {
+      external: ['svelte', 'ask-for-promise'],
+      output: {
+        globals: {
+          svelte: 'svelte',
+          'ask-for-promise': 'askForPromise'
+        }
+      }
+    }
+  }
 })
