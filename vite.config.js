2b2a750cbb183918fd019bf895bc9efdaaa939da
Peter Naydenov <peter.naydenov@gmail.com>
2026-08-04 07:31:58 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,31 +1,21 @@
 import { defineConfig } from 'vite'
 import { svelte } from '@sveltejs/vite-plugin-svelte'
-import { resolve } from 'path'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [svelte()],
+  publicDir: false,
   build: {
     lib: {
-      entry: resolve(__dirname, 'src/main.js'),
-      name: 'VisualControllerSvelte5',
+      entry: 'src/main.js',
+      name: 'VisualController',
       formats: ['es', 'cjs', 'umd'],
-      fileName: (format) => {
-        const map = {
-          es: 'visualController-svelte-5.esm.mjs',
-          cjs: 'visualController-svelte-5.cjs',
-          umd: 'visualController-svelte-5.umd.js'
-        }
-        return map[format]
-      }
+      fileName: (format) => `visual-controller-for-svelte5.${format === 'es' ? 'esm.mjs' : format === 'umd' ? 'umd.js' : 'cjs'}`
     },
     rollupOptions: {
       external: ['svelte', 'ask-for-promise'],
       output: {
-        globals: {
-          svelte: 'svelte',
-          'ask-for-promise': 'askForPromise'
-        }
+        dir: 'dist',
+        globals: { svelte: 'svelte', 'ask-for-promise': 'askForPromise' }
       }
     }
   }
